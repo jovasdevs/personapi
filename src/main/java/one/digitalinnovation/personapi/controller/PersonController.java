1c535aee583defd9a5664828cdfe9446c790cd09
jovasdevs <jovasdevs@gmail.com>
2021-10-19 10:10:56 -0300

    Developed list all method

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -29,5 +30,9 @@ public class PersonController {
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
 
+    }
+
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
     }
 }
