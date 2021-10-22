c3c328b2e76ce7790990c72a3bd38c20c7f9794c
jovasdevs <jovasdevs@gmail.com>
2021-10-22 19:48:35 -0300

    Add README.md

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personapi.controller;
 
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.exception.PersonNotFoundException;
@@ -10,20 +11,14 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-//Adicionando um comentário
+
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-
-        this.personService = personService;
-    }
-
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
